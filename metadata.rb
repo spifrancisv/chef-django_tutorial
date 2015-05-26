942541d337f249edcb909935a81af19a9569dc69
Francis Vidal <francis.vidal@spi-global.com>
2015-05-27 01:35:05 +0800

    Updated metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures django_tutorial'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends "application", "~> 3.0"
+depends "application", "~> 4.1.6"
 depends 'application_python'
 depends 'application_nginx'
 depends 'database'
